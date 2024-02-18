7a08d99a1490bdbc58c85c2b206617cdefbcf310
Cobble8 <drewgenualdo@gmail.com>
2024-02-18 15:40:51 -0500

    final commit

--- Platformer_AGenualdo/Assets/Scripts/MenuScript.cs
+++ Platformer_AGenualdo/Assets/Scripts/MenuScript.cs
@@ -7,7 +7,8 @@ public class MenuScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Persistent.hasMoved = false;
+        Persistent.hasMoused = false;
     }
 
     // Update is called once per frame
